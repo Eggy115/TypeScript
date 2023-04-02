16062ba23851682511e73e358d496e6b56d17943
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-02 18:19:04 +0100

    Create basic types.ts

---
+++ basic types.ts
@@ -0,0 +1,11 @@
+let message: string = "Hello, world!";
+let count: number = 5;
+let isDone: boolean = false;
+let list: number[] = [1, 2, 3];
+let tuple: [string, number] = ["hello", 5];
+
+console.log(message);
+console.log(count);
+console.log(isDone);
+console.log(list);
+console.log(tuple);
